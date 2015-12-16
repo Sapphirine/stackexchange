9bd786c6ce4c04a54638b80c568aa970b7a626af
Xinli Wang <XW@dyn-129-236-216-229.dyn.columbia.edu>
2015-12-16 18:21:43 -0500

    topics with weights

--- get_10_topics.py
+++ get_10_topics.py
@@ -1,42 +1,45 @@
 
 # coding: utf-8
 
-# In[101]:
+# In[143]:
 
 import pandas as pd
+import numpy as np
 
 
-# In[102]:
+# In[144]:
 
 df = pd.read_csv('/Users/XW/Desktop/datascience.stackexchange.com/result.csv',header = None)
 
 
-# In[103]:
+# In[145]:
 
 #df.shape
 
 
-# In[104]:
+# In[146]:
 
 df2 = pd.read_csv('/Users/XW/Desktop/datascience.stackexchange.com/matrix0.csv')
 
 
-# In[105]:
+# In[147]:
 
 words = list(df2.columns.values)
 
 
-# In[106]:
+# In[150]:
 
 topics = pd.DataFrame()    
 for i in range(10):
     flag = df.sort_index(by=[i], ascending=[False])
+    value = flag[i][0:10]
     Ind = flag.index.values[:10]
     Words = [words[i] for i in Ind]
-    topics[i] = pd.Series(Words)
+    topics[2*i] = pd.Series(Words)
+    topics[2*i+1] = pd.Series(np.array(value))
 
 
-# In[107]:
+# In[151]:
 
 topics
 
